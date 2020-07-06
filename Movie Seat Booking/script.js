1b76157c1761ac500ed3881d9b4e677fd8517a3c
talhazubair203 <67759134+talhazubair320@users.noreply.github.com>
2020-07-06 10:14:27 -0700

    Add files via upload

---
+++ Movie Seat Booking/script.js
@@ -0,0 +1,44 @@
+const container = document.querySelector('.container');
+const container= document.querySelectorAll('.row.seat:not(.occupied)');
+const count= document.getElementById('count');
+const total =document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+populateUI();
+
+
+let ticketPrice = +movieSelect.value;
+//update total and count
+
+function updateSelectedCount(){
+    const selectedSeats=document.querySelectorAll('.row.seat.selected');
+const selectedSeatsCount= selectedSeats.length;
+
+count.innerText=selectedSeatsCount;
+total.innerText=selectedSeatsCount * ticketPrice;
+}
+
+function populateUI{
+    const selectedSeats = 
+}
+
+//moive select event
+
+movieSelect.addEventListener('change', e=>{
+     ticketPrice = +e.target.value;
+     updateSelectedCount();
+});
+
+
+//seat click event
+const ticketPrice = +movieSelect.value;
+
+container,addEventListener('click',e =>{
+
+    if (e.target.classList.contains('seat')&&
+    !e.target.classList.contains('occupied')
+    ) {
+        e.target.classList.toggle('selected');
+    }
+});
+
