89fd5595dce8c5d7486e3d0793ebec6d71d54478
talhazubair203 <67759134+talhazubair320@users.noreply.github.com>
2020-07-06 10:13:40 -0700

    Add files via upload

---
+++ Love calcular/index.js
@@ -0,0 +1,6 @@
+function rand() {
+  var a =document.getElementById('i2').value;
+  var b =document.getElementById('i3').value;
+  var c = Math.floor(Math.random()*100);
+  alert(a+" loves with "+b+" "+c+"%");
+}
